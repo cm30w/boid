b33c25cb448a175c126d7277a57f633f1648b23b
Claire Wang <claire.wang368@gmail.com>
2025-12-24 00:31:17 -0500

    first cut of separation and cohesion force

--- boidscript.js
+++ boidscript.js
@@ -30,26 +30,74 @@ class Boid {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.vx = (Math.random() - 0.5) * 60; // speed: pixels per second
-        this.vy = (Math.random() - 0.5) * 60;
-        this.ax = 0;  // acceleration RESERVE FOR FUTURE
+        this.vx = (Math.random() - 0.5) * 90; // speed: pixels per second
+        this.vy = (Math.random() - 0.5) * 90;
+        this.ax = 0;  // acceleration (force)
         this.ay = 0;
         
         // parameters
-        this.maxSpeed = 60;
-        this.minSpeed = 15;  // birds must keep moving
-        this.maxForce = 0.05;
-        this.separationDistance = 25;  // How close is "too close"
+        this.maxSpeed = 90;
+        this.minSpeed = 60;  // birds must keep moving
+        this.maxForce = 0.1;
+        this.separationDistance = 30;  // How close is "too close"
 
         // TODO: make sure vx vy within max and min
         // this.vx = Math.max(this.vs, this.minSpeed);
         // this.vy = Math.max(this.vy, this.minSpeed);
 
-        this.neighbourRadius = 100; // bird can see 
+        this.neighborRadius = 100; // bird can see 
     }
     
-    separate(deltaTime) {
-
+    separate(deltaTime, neighborBoids) {
+        let steerX = 0;
+        let steerY = 0;
+        let count = 0;
+        
+        // check each neighbor within separation distance
+        for (let other of neighborBoids) {
+            const d = distance(this.x, this.y, other.x, other.y);
+            
+            // only consider boids that are too close
+            if (d < this.separationDistance && d > 0) {
+                // v1: repulse_force_vector
+                let v1X = this.x - other.x;
+                let v1Y = this.y - other.y;
+                
+                // v2: normalize(v1) - get repulse force direction
+                const v2 = normalize(v1X, v1Y);
+                
+                // v3: v2 / distance - distance between two boids: it reach infinite as two boids at same position
+                // closer boids create stronger repulsion
+                let v3X = v2.x / d;
+                let v3Y = v2.y / d;
+                
+                // Add to sum
+                steerX += v3X;
+                steerY += v3Y;
+                count++;
+            }
+        }
+        
+        // v4: SUM(v3) / count - total repulsive force. count=num of neighboring boids
+        if (count > 0) {
+            steerX /= count;
+            steerY /= count;
+            
+            // F_SEP: normalize(v4) * maxSpeed - final separation force
+            const norm = normalize(steerX, steerY);
+            steerX = norm.x * this.maxSpeed;
+            steerY = norm.y * this.maxSpeed;
+            
+            // calculate steering force: desired - current
+            steerX = steerX - this.vx;
+            steerY = steerY - this.vy;
+            
+            // limit the force
+            const limited = limit(steerX, steerY, this.maxForce);
+            return limited;
+        }
+        
+        return { x: 0, y: 0 };
     }
 
     align(deltaTime, neighborBoids) {
@@ -86,7 +134,34 @@ class Boid {
 
     }
 
-    cohere(deltaTime) {
+    cohere(deltaTime, neighborBoids) {
+        let avgX = 0;
+        let avgY = 0;
+    
+        for(let other of neighborBoids){
+            avgX += other.x;
+            avgY += other.y;
+        }
+    
+        if (neighborBoids.length > 0) {
+            avgX /= neighborBoids.length;
+            avgY /= neighborBoids.length;
+    
+            // normalize and scale to max speed (desired velocity)
+            const norm = normalize(avgX - this.x, avgY - this.y);
+            let avgVX = norm.x * this.maxSpeed;
+            let avgVY = norm.y * this.maxSpeed;
+            
+            // calculate steering force: desired - current (F_cohere)
+            let steerX = avgVX - this.vx;
+            let steerY = avgVY - this.vy;
+            
+            // limit the force
+            const limited = limit(steerX, steerY, this.maxForce);
+            return limited;
+        }
+        
+        return { x: 0, y: 0 };
     }
 
     getNeighborBoids(perceptionRadius) {
@@ -99,12 +174,14 @@ class Boid {
     }
         
     update(deltaTime) {
-        let neighborBoids = this.getNeighborBoids(this.neighbourRadius);
+        let neighborBoids = this.getNeighborBoids(this.neighborRadius);
         let ali = this.align(deltaTime, neighborBoids); // align
+        let coh = this.cohere(deltaTime, neighborBoids); // cohere
+        let sep = this.separate(deltaTime, neighborBoids); // separate
 
         // combine forces
-        this.ax = ali.x * 2.0; // todo: adjust weighting later when other forces are here
-        this.ay = ali.y * 2.0;
+        this.ax = ali.x + coh.x + sep.x * 3.0; // todo: adjust weighting later when other forces are here
+        this.ay = ali.y + coh.y + sep.y * 3.0;
     
         // Update velocity with acceleration
         this.vx += this.ax;
